8398dc9d7c29ff71313ecb627e0023c4f09a7749
Keith Sloan <keith@sloan-home.co.uk>
2020-10-14 19:17:15 +0100

    extract part 1

--- Utils/extractVol.py
+++ Utils/extractVol.py
@@ -1,6 +1,41 @@
 import sys
 from lxml import etree 
 
+volList = []
+solidList = []
+positionList = []
+rotationList = []
+
+def processVol(vol) :
+   global volList, solidList
+   print(vol)
+   #print(vol.attrib)
+   vname = vol.attrib.get('name')
+   print('volume : ' + vname)
+   if vname not in volList : volList.append(vname)
+   solid = vol.find('solidref')
+   #print(solid.attrib)
+   sname = solid.attrib.get('ref')
+   print('solid : ' + sname)
+   if sname not in solidList : solidList.append(sname)
+   material = vol.find('materialref')
+   if material is not None :
+      #print('material : '+str(material.attrib))
+      print('material : ' + material.attrib.get('ref'))
+   for pv in vol.findall('physvol') :
+      volref = pv.find('volumeref')
+      print('physvol : '+volref.attrib.get('ref'))
+      posref = pv.find('positionref')
+      if posref is not None :
+         posname = posref.attrib.get('ref')
+         print('Position ref : '+posname)
+         if posname not in positionList : positionList.append(posname)
+      rotref = pv.find('rotationref')
+      if rotref is not None :
+         rotname = rotref.attrib.get('ref')
+         print('Rotation ref : '+rotname)
+         if rotname not in rotationList : rotationList.append(rotname)
+
 if len(sys.argv)<4:
   print ("Usage: sys.argv[0] <Volume> <in_file> <Out_file")
   sys.exit(1)
@@ -18,34 +53,15 @@ structure = tree.find('structure')
 #vol = structure.find('volume[@name="World"]')
 vol = structure.find(f"volume[@name='{volume}']")
 if vol is not None :
-   print(vol)
-   print(vol.attrib)
-   print(vol.attrib.get('name'))
-   solid = vol.find('solidref')
-   print(solid.attrib)
-   print(solid.attrib.get('ref'))
-   material = vol.find('materialref')
-   if material is not None :
-      print('material : '+str(material.attrib))
-      print(material.attrib.get('ref'))
-   for pv in vol.findall('physvol') :
-      volref = pv.find('volumeref')
-      print(volref.attrib)
-      posref = pv.find('positionref')
-      if posref is not None :
-         print(posref.attrib.get('ref'))
-      rotref = pv.find('rotationref')
-      if rotref is not None :
-         print(rotref.attrib.get('ref'))
+   processVol(vol)
 else :
    print('Volume : '+volume+' Not found')
-#solidList = []
-#for s in tree.xpath('//solids/*') :
-#    #print(s.tag)
-#    if s.tag not in solidList :
-#        solidList.append(s.tag)
-#l = 'List of Solids in : '+iname
-#print(l)
-#print('=' * len(l))
-#for i in solidList :
-#    print(i)        
+
+print('Vol List')
+print(volList)
+print('Solid List')
+print(solidList)
+print('Position List')
+print(positionList)
+print('Rotation List')
+print(rotationList)

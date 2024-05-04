95ed2d94429dc30e300cb7a4a49b94eb33fcaf51
Keith Sloan <keith@sloan-home.co.uk>
2024-05-04 20:34:14 +0100

    Fix for FC 0.21.1 save/restore

--- freecad/gdml/GDMLObjects.py
+++ freecad/gdml/GDMLObjects.py
@@ -437,8 +437,12 @@ class GDMLsolid:
         we must define this method\
         to return a tuple of all serializable objects or None."""
         if hasattr(self, "Type"):
-            #print(f"getstate : Type {self.Type}")
+            print(f"getstate : Type {self.Type}")
             return {"type": self.Type}
+        elif hasattr(self.Proxy, "Type"):
+            print(f"getstate : Type {self.Proxy.Type}")
+            return {"type": self.Proxy.Type}
+
         else:
             print(f"Error GDMLsolid should have Type")
             #print(f" self {self}")
